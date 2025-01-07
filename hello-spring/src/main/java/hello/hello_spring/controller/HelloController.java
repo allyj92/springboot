b71d22a79db007413b4e17606783508c46b463db
eunjaelim <ieunjae361@gmail.com>
2025-01-07 23:17:52 +0900

    view 환경설정

---
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,12 @@
+package hello.hello_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+    public String hello(Model model){
+        model.addAttribute("data","hello!");
+        return "hello";
+    }
+}
