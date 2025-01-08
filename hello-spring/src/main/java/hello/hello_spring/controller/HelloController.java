f77c39a7da37b446890cd5d3bda22d6400bd9351
eunjaelim <ieunjae361@gmail.com>
2025-01-08 18:25:01 +0900

    초기설정

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
@@ -2,11 +2,14 @@ package hello.hello_spring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    public String hello(Model model){
-        model.addAttribute("data","hello!");
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
         return "hello";
     }
 }
