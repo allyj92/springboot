18f2daa04db81dbfe8d3e50307cffd8704e7eca2
eunjaelim <ieunjae361@gmail.com>
2025-01-13 16:16:33 +0900

    hello servlet

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -14,7 +14,14 @@ import java.io.IOException;
 public class HelloServlet extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+        response.getWriter().write("hello " + username);
+        
     }
 }
