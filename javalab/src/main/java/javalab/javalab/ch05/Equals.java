338e1b72eed4f09a6f3e25746275f5612911ac30
eunjaelim <ieunjae361@gmail.com>
2025-07-14 09:49:32 +0900

    equals

---
+++ javalab/src/main/java/javalab/javalab/ch05/Equals.java
@@ -0,0 +1,13 @@
+package javalab.javalab.ch05;
+
+public class Equals {
+    public static void main(String[] args) {
+        Integer a = 1000;
+        Integer b = 1000;
+
+        System.out.println(a.equals(b));
+
+    }
+
+
+}
